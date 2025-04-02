cf9f53f3cb7fe50ffd5737922ce9a2d14566c958
Chanchal Alam <106960500+chanchalalam@users.noreply.github.com>
2025-04-02 16:19:11 +0530

    Update recognition_module.py

--- recognition_module.py
+++ recognition_module.py
@@ -34,20 +34,24 @@ from webcolors import (
 import os
 import tensorflow as tf
 
-# Get the base directory (where your script is running)
+from keras.layers import TFSMLayer
+import os
+
+# Get base directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Define model directory paths
+# Define model paths
 sub_model_path = os.path.join(BASE_DIR, "models", "model_sub")
 top_model_path = os.path.join(BASE_DIR, "models", "model_top")
 bottom_model_path = os.path.join(BASE_DIR, "models", "model_bottom")
 foot_model_path = os.path.join(BASE_DIR, "models", "model_shoes")
 
-# Load models (SavedModel format)
-sub_model = tf.keras.models.load_model(sub_model_path)
-top_model = tf.keras.models.load_model(top_model_path)
-bottom_model = tf.keras.models.load_model(bottom_model_path)
-foot_model = tf.keras.models.load_model(foot_model_path)
+# Load models using TFSMLayer
+sub_model = TFSMLayer(sub_model_path, call_endpoint='serving_default')
+top_model = TFSMLayer(top_model_path, call_endpoint='serving_default')
+bottom_model = TFSMLayer(bottom_model_path, call_endpoint='serving_default')
+foot_model = TFSMLayer(foot_model_path, call_endpoint='serving_default')
+
 
 
 # # Load pre-trained models
